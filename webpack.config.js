6db9a6d77bf64ea300a391a1d0265adfc38a92c5
Daniel Kovac <dkovac@logitech.com>
2019-03-06 11:05:15 -0800

    Injecting the script into the index.html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
@@ -18,6 +19,10 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
                 exclude: /node_modules/,
                 use: [
                     { loader: 'style-loader' },
@@ -28,21 +33,21 @@ module.exports = {
                             modules: true,
                             localIdentName: '[name]__[local]__[hash:base64:5]'
                         }
-                    },
-                    { 
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: () => [
-                                autoprefixer({
-                                    "browsers": [
+                     },
+                     { 
+                         loader: 'postcss-loader',
+                         options: {
+                             ident: 'postcss',
+                             plugins: () => [
+                                 autoprefixer({
+                                     browsers: [
                                         "> 1%",
                                         "last 2 versions"
-                                    ]
-                                })
-                            ]
-                        }
-                    }
+                                     ]
+                                 })
+                             ]
+                         }
+                      }
                 ]
             },
             {
@@ -50,5 +55,12 @@ module.exports = {
                 loader: 'url-loader?limit=8000&name=images/[name].[ext]'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
 };
