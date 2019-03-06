b3ca4e2c03427d21e00a8b443255edc6ba48bd70
Daniel Kovac <dkovac@logitech.com>
2019-03-06 11:11:52 -0800

    Fixing packages for building

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 
 import Users from './containers/Users';
 import asyncComponent from './hoc/asyncComponent';
@@ -13,8 +13,7 @@ class App extends Component {
         return (
             <div>
                 <div>
-                    <Link to="/">Users</Link>
-                    <Link to="/pizza">Pizza</Link>
+                    <Link to="/">Users</Link> | <Link to="/pizza">Pizza</Link>
                 </div>
                 <div>
                     <Route path="/" exact component={Users} />
